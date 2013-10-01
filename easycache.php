97284f2dd7da71128031dbc8c9e6ba34835c0f48
kruemel <soeren@siegismund-poschmann.de>
2013-10-01 10:19:23 +0200

    test

---
+++ easycache.php
@@ -0,0 +1,65 @@
+<?php
+namespace bitWeb;
+class easycache
+{
+	private $cache = false;
+	public function __construct()
+	{
+		$c = new config();
+		if (function_exists('apc_add')&&$c->get("caching"))
+		{
+			$this->cache=true;
+		}
+	}
+	public function add($key, $value, $ttl = 120)
+	{
+		if($this->cache)
+		{
+			apc_add($key, $value, $ttl);
+		}
+	}
+	public function get($key)
+	{
+		if($this->cache)
+		{
+			if(apc_exists($key))
+			{
+				return apc_fetch($key);
+			}
+		}
+		return false;
+	}
+	public function del($key)
+	{
+		if($this->cache)
+		{
+			if(apc_exists($key))
+			{
+				apc_delete($key);
+			}
+		}
+	}
+	public function clear()
+	{
+		if($this->cache)
+		{
+			apc_clear_cache('user');
+		}
+	}
+	public function useCache()
+	{
+		if($this->cache)
+		{
+			return true;
+		}
+		return false;
+	}
+	public function info()
+	{
+		if($this->cache)
+		{
+			return apc_cache_info("user");
+		}
+	}
+}
+?>
